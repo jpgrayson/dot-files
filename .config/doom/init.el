96daefbb2e841a90e0e7e7e08fa11265ccc7d85e
Peter Grayson <pete@jpgrayson.net>
2023-08-23 16:00:28 -0400

    doom: disable cython

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -160,7 +160,7 @@
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        ;;python            ; beautiful is better than ugly
-       (python +lsp +pyright +cython)
+       (python +lsp +pyright)
        ;;(python +pyenv)
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
