57646a365342d62ab7d76c80599b1e2b15cdc86c
Peter Grayson <pete@jpgrayson.net>
2023-08-30 15:37:53 -0400

    nvim: lazy, space leader, etc.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,48 +1,107 @@
-local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  vim.api.nvim_command(
-    '!git clone --depth=1 https://github.com/wbthomason/packer.nvim ' ..  install_path
-  )
-  vim.cmd 'packadd packer.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
--- Only if your version of Neovim doesn't have https://github.com/neovim/neovim/pull/12632 merged
---vim._update_package_paths()
+require("lazy").setup({
+  {
+    'mrjones2014/legendary.nvim',
+    priority = 10000,
+    lazy = false,
+    opts = { which_key = { auto_register = true } }
+  },
 
-require('packer').startup(function (use)
-  use 'wbthomason/packer.nvim'
-
-  use 'tpope/vim-commentary'
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-rsi'
-  use 'tpope/vim-sleuth'
-  use 'tpope/vim-surround'
-
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = {
-      {'nvim-lua/popup.nvim'},
-      {'nvim-lua/plenary.nvim'}
-    }
-  }
-
-  use 'nvim-treesitter/nvim-treesitter'
-
-  use 'joshdick/onedark.vim'
-  use 'itchyny/lightline.vim'
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/nvim-compe'
-  use 'qpkorr/vim-bufkill'
-
-  use {
+  {
     'folke/which-key.nvim',
-    config = function()
+    init = function()
       vim.o.timeout = true
       vim.o.timeoutlen = 300
-      require("which-key").setup {}
+    end,
+    opts = {},
+  },
+
+  {'navarasu/onedark.nvim', lazy = true, opts = { style = 'cool' }},
+  {'tpope/vim-commentary'},
+  {'tpope/vim-fugitive'},
+  {'tpope/vim-rsi'},
+  {'tpope/vim-sleuth'},
+  {'qpkorr/vim-bufkill'},
+
+  { -- Reimplementation of tpope/vim-surround
+    'kylechui/nvim-surround',
+    event = "VeryLazy",
+    config = function()
+      require'nvim-surround'.setup({
+        opts = {
+          keymaps = {
+            -- Unmap C-g from insert mode
+            insert = nil,
+            insert_line = nil,
+          },
+        },
+      })
     end
-  }
-end)
+  },
+
+
+  { -- Fuzzy finder over lists.
+    'nvim-telescope/telescope.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {},
+  },
+
+  {'nvim-treesitter/nvim-treesitter'},
+  {'stevearc/dressing.nvim', opts = {}},
+  {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+    },
+  },
+
+  {
+    'neovim/nvim-lspconfig',
+    keys = {
+      -- { '<space>e', mode = { "n" }, vim.diagnostic.open_float, desc = "LSP Diag Float" },
+      -- { '<space>q', mode = { "n" }, vim.diagnostic.setloclist, desc = "Open LSP Diags" },
+    },
+
+  },
+
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {
+      modes = {
+        search = {
+          enabled = false,
+        },
+      },
+    },
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash" },
+      -- { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
+
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {},
+  },
+})
 
 vim.g.mapleader = ' '
 
@@ -73,39 +132,100 @@ vim.opt.wildignore = {
 }
 
 vim.o.termguicolors = true
-vim.g.onedark_terminal_italics = 2
 vim.cmd[[colorscheme onedark]]
-
-vim.g.lightline = {
-  colorscheme = 'onedark';
-  active = {
-    left = {
-      {'mode', 'paste'},
-      {'gitbranch', 'readonly', 'filename', 'modified'},
-    },
-    right = {
-      {'lineinfo'},
-      {'percent'},
-      {'fileformat', 'fileencoding', 'filetype'},
-    }
-  };
-  inactive = {
-    left = {
-      {'modified', 'relativepath'},
-    },
-    right = {
-      {'lineinfo'},
-      {'percent'},
-    },
-  };
-  component_function = {
-    gitbranch = 'fugitive#head',
-  };
-}
-
 vim.o.completeopt = 'menuone,noinsert'
 
-require('compe').setup {}
+-- require('legendary').setup({
+--   keymaps = {
+--     {
+--       itemgroup = "file ID",
+--       description = "file...",
+--       icon = "",
+--       keymaps = {
+--         { '<leader>ff', ':Telescope find_files<CR>', description = 'Find files' },
+--         { '<leader>fs', ':w<CR>', description = 'Save file' },
+--       },
+--     },
+--     { '<leader>cj', ':Telescope lsp_document_symbols<CR>', desc = "LSP Document Symbols" },
+--     { '<leader>cd', ':Telescope lsp_definitions<CR>', desc = "Jump to definition" },
+--     { '<leader>cJ', ':Telescope lsp_references<CR>', desc = "Jump to references" },
+--     { '<leader>qq', ':qall<CR>', desc = "Exit Vim" },
+--     { '<leader>ts', function() require'onedark'.toggle() end, desc = "Toggle Style" },
+--   },
+-- })
+
+local toggle_line_numbering = function() 
+  if not vim.o.number and not vim.o.relativenumber then
+    vim.o.number = true
+  elseif vim.o.number and not vim.o.relativenumber then
+    vim.o.relativenumber = true
+  elseif vim.o.relativenumber then
+    vim.o.number = false
+    vim.o.relativenumber = false
+  end
+end
+
+local toggle_read_only = function()
+  vim.bo.readonly = not vim.bo.readonly
+  vim.bo.modifiable = not vim.bo.readonly
+end
+
+require('which-key').register({
+  ['<leader>'] = {
+    b = {
+      name = '+buffer',
+      d = { '<cmd>BD<cr>', 'Kill buffer' },
+      k = { '<cmd>BD<cr>', 'Kill buffer' },
+      n = { '<cmd>bnext<cr>', 'Next buffer' },
+      p = { '<cmd>bprev<cr>', 'Prev buffer' },
+      N = { '<cmd>enew<cr>', 'New buffer' },
+    },
+    c = {
+      name = '+code',
+      j = { '<cmd>Telescope lsp_document_symbols<cr>', 'LSP Document Symbols' },
+      d = { '<cmd>Telescope lsp_definitions<cr>', 'Jump to definition' },
+      J = { '<cmd>Telescope lsp_references<cr>', 'Jump to references' },
+    },
+    f = {
+      name = '+file',
+      f = { '<cmd>Telescope find_files<cr>', 'Find file' },
+      s = { '<cmd>write<cr>', 'Save file' },
+    },
+    q = {
+      name = '+quit',
+      q = { '<cmd>qall<cr>', 'Exit Vim' },
+    },
+    t = {
+      name = '+toggle',
+      c = { function() vim.o.signcolumn = vim.o.signcolumn == 'yes' and 'no' or 'yes' end, 'Sign column' },
+      l = { toggle_line_numbering, 'Line numbers' },
+      r = { toggle_read_only, 'Read-only' },
+      s = { function() require'onedark'.toggle() end, 'Rotate OneDark Style' },
+
+    },
+  }
+})
+
+local lspconfig = require('lspconfig')
+lspconfig.pyright.setup {}
+lspconfig.rust_analyzer.setup {}
+
+local cmp = require'cmp'
+cmp.setup({
+  completion = { autocomplete = false },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-g>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+  }, {
+    { name = 'buffer' },
+  }),
+})
 
 require'nvim-treesitter.configs'.setup {
   ensure_installed = {
