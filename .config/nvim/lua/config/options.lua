cb4c365dca149b9e967ad2b8a583d120046e6b2e
Peter Grayson <pete@jpgrayson.net>
2024-07-22 08:15:58 -0400

    nvim: disable autoformat globally

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set:
 --   https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 
+vim.g.autoformat = false
 vim.opt.backup = false
 vim.opt.writebackup = false
 vim.opt.swapfile = false
--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,6 +2,7 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      -- autoformat = false,
       servers = {
         sourcekit = {
           cmd = { "sourcekit-lsp" },
