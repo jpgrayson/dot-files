e3549562db5746e671b0cc8269e53f0062fd5e86
Peter Grayson <pete@jpgrayson.net>
2024-09-16 08:24:19 -0400

    doom: enable dirvish

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -67,8 +67,9 @@
        ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
-       dired             ; making dired pretty [functional]
+       (dired +dirvish +icons) ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
+       ;;eww               ; the internet is gross
        ibuffer           ; interactive buffer management
        undo              ; persistent, smarter undo for your inevitable mistakes
        vc                ; version-control and Emacs, sitting in a tree
@@ -154,6 +155,7 @@
        (org +journal) ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
+       ;;graphviz          ; diagrams for confusing yourself even more
        ;;purescript        ; javascript, but functional
        ;;python            ; beautiful is better than ugly
        (python +lsp +pyright)
